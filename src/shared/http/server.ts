06e432b14bf5a187913de45ecc4e12c7f416170d
Matheus Marques <matheus.marques@limberchef.com.br>
2024-01-07 03:20:30 -0300

    criação server
    comunicação com o servidor, criação de rotas padrão e tratativa de erro

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,30 @@
-console.log("hello word")
+import express, { NextFunction, Response, Request } from "express";
+import cors from "cors";
+import routes from "./routes";
+import AppError from "./errors/AppError";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if(error instanceof AppError){
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    });
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error'
+  });
+
+});
+
+app.listen(3333, () => {
+  console.log(`Server started on port http://localhost:3333/ !`)
+})
